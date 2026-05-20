1614a7e1ca545c1439a054aebfb802fbc6936286
Carlos Diaz <150292662+charly773@users.noreply.github.com>
2026-05-20 11:55:54 -0600

    párte2

---
+++ parcial1/Clase1_Calculadora/Form1.Designer.vb
@@ -0,0 +1,304 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Bnt_num1 = New Button()
+        Bnt_num2 = New Button()
+        Bnt_num3 = New Button()
+        Bnt_num4 = New Button()
+        Bnt_num5 = New Button()
+        Bnt_num6 = New Button()
+        Bnt_num7 = New Button()
+        Bnt_num8 = New Button()
+        Bnt_num9 = New Button()
+        Bnt_num0 = New Button()
+        suma = New Button()
+        resta = New Button()
+        multiplica = New Button()
+        divide = New Button()
+        Btn_elimina_caracter = New Button()
+        btnClear = New Button()
+        igual = New Button()
+        pantallauno = New ListBox()
+        pantallados = New ListBox()
+        Btnparenteci1 = New Button()
+        Btnparenteci2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Bnt_num1
+        ' 
+        Bnt_num1.BackColor = SystemColors.ButtonFace
+        Bnt_num1.Location = New Point(78, 203)
+        Bnt_num1.Name = "Bnt_num1"
+        Bnt_num1.Size = New Size(75, 23)
+        Bnt_num1.TabIndex = 0
+        Bnt_num1.Text = "1"
+        Bnt_num1.UseVisualStyleBackColor = False
+        ' 
+        ' Bnt_num2
+        ' 
+        Bnt_num2.BackColor = SystemColors.ButtonFace
+        Bnt_num2.Location = New Point(176, 203)
+        Bnt_num2.Name = "Bnt_num2"
+        Bnt_num2.Size = New Size(75, 23)
+        Bnt_num2.TabIndex = 1
+        Bnt_num2.Text = "2"
+        Bnt_num2.UseVisualStyleBackColor = False
+        ' 
+        ' Bnt_num3
+        ' 
+        Bnt_num3.BackColor = SystemColors.ButtonFace
+        Bnt_num3.Location = New Point(279, 203)
+        Bnt_num3.Name = "Bnt_num3"
+        Bnt_num3.Size = New Size(75, 23)
+        Bnt_num3.TabIndex = 2
+        Bnt_num3.Text = "3"
+        Bnt_num3.UseVisualStyleBackColor = False
+        ' 
+        ' Bnt_num4
+        ' 
+        Bnt_num4.BackColor = SystemColors.ButtonFace
+        Bnt_num4.Location = New Point(78, 232)
+        Bnt_num4.Name = "Bnt_num4"
+        Bnt_num4.Size = New Size(75, 23)
+        Bnt_num4.TabIndex = 3
+        Bnt_num4.Text = "4"
+        Bnt_num4.UseVisualStyleBackColor = False
+        ' 
+        ' Bnt_num5
+        ' 
+        Bnt_num5.BackColor = SystemColors.ButtonFace
+        Bnt_num5.Location = New Point(176, 232)
+        Bnt_num5.Name = "Bnt_num5"
+        Bnt_num5.Size = New Size(75, 23)
+        Bnt_num5.TabIndex = 4
+        Bnt_num5.Text = "5"
+        Bnt_num5.UseVisualStyleBackColor = False
+        ' 
+        ' Bnt_num6
+        ' 
+        Bnt_num6.BackColor = SystemColors.ButtonFace
+        Bnt_num6.Location = New Point(279, 232)
+        Bnt_num6.Name = "Bnt_num6"
+        Bnt_num6.Size = New Size(75, 23)
+        Bnt_num6.TabIndex = 5
+        Bnt_num6.Text = "6"
+        Bnt_num6.UseVisualStyleBackColor = False
+        ' 
+        ' Bnt_num7
+        ' 
+        Bnt_num7.BackColor = SystemColors.ButtonFace
+        Bnt_num7.Location = New Point(78, 261)
+        Bnt_num7.Name = "Bnt_num7"
+        Bnt_num7.Size = New Size(75, 23)
+        Bnt_num7.TabIndex = 6
+        Bnt_num7.Text = "7"
+        Bnt_num7.UseVisualStyleBackColor = False
+        ' 
+        ' Bnt_num8
+        ' 
+        Bnt_num8.BackColor = SystemColors.ButtonFace
+        Bnt_num8.Location = New Point(176, 261)
+        Bnt_num8.Name = "Bnt_num8"
+        Bnt_num8.Size = New Size(75, 23)
+        Bnt_num8.TabIndex = 7
+        Bnt_num8.Text = "8"
+        Bnt_num8.UseVisualStyleBackColor = False
+        ' 
+        ' Bnt_num9
+        ' 
+        Bnt_num9.BackColor = SystemColors.ButtonFace
+        Bnt_num9.Location = New Point(279, 261)
+        Bnt_num9.Name = "Bnt_num9"
+        Bnt_num9.Size = New Size(75, 23)
+        Bnt_num9.TabIndex = 8
+        Bnt_num9.Text = "9"
+        Bnt_num9.UseVisualStyleBackColor = False
+        ' 
+        ' Bnt_num0
+        ' 
+        Bnt_num0.BackColor = SystemColors.ButtonFace
+        Bnt_num0.Location = New Point(279, 290)
+        Bnt_num0.Name = "Bnt_num0"
+        Bnt_num0.Size = New Size(75, 23)
+        Bnt_num0.TabIndex = 9
+        Bnt_num0.Text = "0"
+        Bnt_num0.UseVisualStyleBackColor = False
+        ' 
+        ' suma
+        ' 
+        suma.Location = New Point(433, 200)
+        suma.Name = "suma"
+        suma.Size = New Size(75, 23)
+        suma.TabIndex = 11
+        suma.Text = "suma"
+        suma.UseVisualStyleBackColor = True
+        ' 
+        ' resta
+        ' 
+        resta.Location = New Point(433, 229)
+        resta.Name = "resta"
+        resta.Size = New Size(75, 23)
+        resta.TabIndex = 12
+        resta.Text = "resta"
+        resta.UseVisualStyleBackColor = True
+        ' 
+        ' multiplica
+        ' 
+        multiplica.Location = New Point(433, 261)
+        multiplica.Name = "multiplica"
+        multiplica.Size = New Size(75, 23)
+        multiplica.TabIndex = 13
+        multiplica.Text = "multi"
+        multiplica.UseVisualStyleBackColor = True
+        ' 
+        ' divide
+        ' 
+        divide.Location = New Point(433, 290)
+        divide.Name = "divide"
+        divide.Size = New Size(75, 23)
+        divide.TabIndex = 14
+        divide.Text = "DIV"
+        divide.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_elimina_caracter
+        ' 
+        Btn_elimina_caracter.Location = New Point(433, 319)
+        Btn_elimina_caracter.Name = "Btn_elimina_caracter"
+        Btn_elimina_caracter.Size = New Size(75, 23)
+        Btn_elimina_caracter.TabIndex = 15
+        Btn_elimina_caracter.Text = "Delete"
+        Btn_elimina_caracter.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(433, 348)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(75, 23)
+        btnClear.TabIndex = 16
+        btnClear.Text = "Eliminar"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' igual
+        ' 
+        igual.BackColor = SystemColors.ButtonFace
+        igual.Location = New Point(279, 319)
+        igual.Name = "igual"
+        igual.Size = New Size(75, 23)
+        igual.TabIndex = 17
+        igual.Text = "IGUAL"
+        igual.UseVisualStyleBackColor = False
+        ' 
+        ' pantallauno
+        ' 
+        pantallauno.FormattingEnabled = True
+        pantallauno.ItemHeight = 15
+        pantallauno.Location = New Point(78, 45)
+        pantallauno.Name = "pantallauno"
+        pantallauno.Size = New Size(430, 94)
+        pantallauno.TabIndex = 19
+        ' 
+        ' pantallados
+        ' 
+        pantallados.FormattingEnabled = True
+        pantallados.ItemHeight = 15
+        pantallados.Location = New Point(78, 145)
+        pantallados.Name = "pantallados"
+        pantallados.Size = New Size(430, 49)
+        pantallados.TabIndex = 20
+        ' 
+        ' Btnparenteci1
+        ' 
+        Btnparenteci1.BackColor = SystemColors.ButtonFace
+        Btnparenteci1.Location = New Point(78, 290)
+        Btnparenteci1.Name = "Btnparenteci1"
+        Btnparenteci1.Size = New Size(75, 23)
+        Btnparenteci1.TabIndex = 21
+        Btnparenteci1.Text = "("
+        Btnparenteci1.UseVisualStyleBackColor = False
+        ' 
+        ' Btnparenteci2
+        ' 
+        Btnparenteci2.BackColor = SystemColors.ButtonFace
+        Btnparenteci2.Location = New Point(176, 290)
+        Btnparenteci2.Name = "Btnparenteci2"
+        Btnparenteci2.Size = New Size(75, 23)
+        Btnparenteci2.TabIndex = 22
+        Btnparenteci2.Text = ")"
+        Btnparenteci2.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(593, 450)
+        Controls.Add(Btnparenteci2)
+        Controls.Add(Btnparenteci1)
+        Controls.Add(pantallados)
+        Controls.Add(pantallauno)
+        Controls.Add(igual)
+        Controls.Add(btnClear)
+        Controls.Add(Btn_elimina_caracter)
+        Controls.Add(divide)
+        Controls.Add(multiplica)
+        Controls.Add(resta)
+        Controls.Add(suma)
+        Controls.Add(Bnt_num0)
+        Controls.Add(Bnt_num9)
+        Controls.Add(Bnt_num8)
+        Controls.Add(Bnt_num7)
+        Controls.Add(Bnt_num6)
+        Controls.Add(Bnt_num5)
+        Controls.Add(Bnt_num4)
+        Controls.Add(Bnt_num3)
+        Controls.Add(Bnt_num2)
+        Controls.Add(Bnt_num1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Bnt_num1 As Button
+    Friend WithEvents Bnt_num2 As Button
+    Friend WithEvents Bnt_num3 As Button
+    Friend WithEvents Bnt_num4 As Button
+    Friend WithEvents Bnt_num5 As Button
+    Friend WithEvents Bnt_num6 As Button
+    Friend WithEvents Bnt_num7 As Button
+    Friend WithEvents Bnt_num8 As Button
+    Friend WithEvents Bnt_num9 As Button
+    Friend WithEvents Bnt_num0 As Button
+    Friend WithEvents suma As Button
+    Friend WithEvents resta As Button
+    Friend WithEvents multiplica As Button
+    Friend WithEvents divide As Button
+    Friend WithEvents Btn_elimina_caracter As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents igual As Button
+    Friend WithEvents pantallauno As ListBox
+    Friend WithEvents pantallados As ListBox
+    Friend WithEvents Btnparenteci1 As Button
+    Friend WithEvents Btnparenteci2 As Button
+
+End Class
---
+++ parcial1/Clase1_Calculadora/Form1.vb
@@ -0,0 +1,113 @@
+﻿Imports System.Data
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private pantallaTexto As String = ""
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        pantallauno.Items.Clear()
+        pantallados.Items.Clear()
+    End Sub
+
+    Private Sub AgregarNumero(valor As String)
+        pantallaTexto &= valor
+        ActualizarPantalla()
+    End Sub
+
+    Private Sub Bnt_num0_Click(sender As Object, e As EventArgs) Handles Bnt_num0.Click
+        AgregarNumero("0")
+    End Sub
+    Private Sub Bnt_num1_Click(sender As Object, e As EventArgs) Handles Bnt_num1.Click
+        AgregarNumero("1")
+    End Sub
+    Private Sub Bnt_num2_Click(sender As Object, e As EventArgs) Handles Bnt_num2.Click
+        AgregarNumero("2")
+    End Sub
+    Private Sub Bnt_num3_Click(sender As Object, e As EventArgs) Handles Bnt_num3.Click
+        AgregarNumero("3")
+    End Sub
+    Private Sub Bnt_num4_Click(sender As Object, e As EventArgs) Handles Bnt_num4.Click
+        AgregarNumero("4")
+    End Sub
+    Private Sub Bnt_num5_Click(sender As Object, e As EventArgs) Handles Bnt_num5.Click
+        AgregarNumero("5")
+    End Sub
+    Private Sub Bnt_num6_Click(sender As Object, e As EventArgs) Handles Bnt_num6.Click
+        AgregarNumero("6")
+    End Sub
+    Private Sub Bnt_num7_Click(sender As Object, e As EventArgs) Handles Bnt_num7.Click
+        AgregarNumero("7")
+    End Sub
+    Private Sub Bnt_num8_Click(sender As Object, e As EventArgs) Handles Bnt_num8.Click
+        AgregarNumero("8")
+    End Sub
+    Private Sub Bnt_num9_Click(sender As Object, e As EventArgs) Handles Bnt_num9.Click
+        AgregarNumero("9")
+    End Sub
+
+    Private Sub AgregarOperacion(op As String)
+        If pantallaTexto <> "" Then
+            pantallaTexto &= op
+            ActualizarPantalla()
+        End If
+    End Sub
+
+    Private Sub suma_Click(sender As Object, e As EventArgs) Handles suma.Click
+        AgregarOperacion("+")
+    End Sub
+    Private Sub resta_Click(sender As Object, e As EventArgs) Handles resta.Click
+        AgregarOperacion("-")
+    End Sub
+    Private Sub multiplica_Click(sender As Object, e As EventArgs) Handles multiplica.Click
+        AgregarOperacion("*")
+    End Sub
+    Private Sub divide_Click(sender As Object, e As EventArgs) Handles divide.Click
+        AgregarOperacion("/")
+    End Sub
+
+    Private Sub Btnparenteci1_Click(sender As Object, e As EventArgs) Handles Btnparenteci1.Click
+        pantallaTexto &= "("
+        ActualizarPantalla()
+    End Sub
+    Private Sub Btnparenteci2_Click(sender As Object, e As EventArgs) Handles Btnparenteci2.Click
+        pantallaTexto &= ")"
+        ActualizarPantalla()
+    End Sub
+
+    Private Function PrepararExpresion(expresion As String) As String
+        Dim nuevoTexto As String = Regex.Replace(expresion, "\)\(", ")*(")
+        nuevoTexto = Regex.Replace(nuevoTexto, "\)(\d)", ")*$1")
+        nuevoTexto = Regex.Replace(nuevoTexto, "(\d)\(", "$1*(")
+        Return nuevoTexto
+    End Function
+
+    Private Sub igual_Click(sender As Object, e As EventArgs) Handles igual.Click
+        Try
+            Dim dt As New DataTable()
+            Dim expresionPreparada = PrepararExpresion(pantallaTexto)
+            Dim resultado = dt.Compute(expresionPreparada, "")
+            pantallados.Items.Clear()
+            pantallados.Items.Add(resultado.ToString())
+        Catch ex As Exception
+            MessageBox.Show("Expresión inválida")
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        pantallaTexto = ""
+        pantallauno.Items.Clear()
+        pantallados.Items.Clear()
+    End Sub
+
+    Private Sub Btn_elimina_caracter_Click(sender As Object, e As EventArgs) Handles Btn_elimina_caracter.Click
+        If pantallaTexto.Length > 0 Then
+            pantallaTexto = pantallaTexto.Substring(0, pantallaTexto.Length - 1)
+            ActualizarPantalla()
+        End If
+    End Sub
+
+    Private Sub ActualizarPantalla()
+        pantallauno.Items.Clear()
+        pantallauno.Items.Add(pantallaTexto)
+    End Sub
+End Class
